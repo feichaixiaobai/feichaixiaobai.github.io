ec4888ca0523caca5063ca380c1dd3cad9a2af5d
feichaixiaobai <lilongbetter@gmail.com>
2026-02-11 00:43:25 +0800

    Site updated: 2026-02-11 00:43:25

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1770741659298";const preCache = ["/images/taichi.png","/images/banner.png","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1770741803337";const preCache = ["/images/taichi.png","/images/banner.png","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
